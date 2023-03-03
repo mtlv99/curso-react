fb27208462e0019ff25362ce501f8a5f77a133ae
Marco León <marco.tulio.leon@gmail.com>
2023-03-03 17:54:21 -0600

    Section 21: lesson 323 - unit test config

---
+++ 08-journal-app/section-20/jest.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  // No es necesario este setup file actualmente, solo si se usa el polyfill del fetch API
+  // en versiones de Node anteriores.
+  setupFiles: ['./jest.setup.js'],
+};
